88a0b76b61ecb9093cde378f956a91b27450fcb9
Mohamed Ragab <63867404+mr28374655@users.noreply.github.com>
2021-12-24 10:54:18 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,458 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+// int main()
+// {
+//     // int i = 5 ,j = 3;
+//     // float div = (float) i / j;
+//     // cout << div;
+
+// //     int x = 5 ,y = 5 ,z = 5 ,a = 5;
+// //     (--x);
+// //     ++y;
+// //     z++;
+// //     a--;
+// //     cout << x << "\n";
+// //     cout << a << "\n";
+// //     cout << z << "\n";
+// //     cout << y;
+
+
+
+
+
+//     // int num;
+    
+//     // cout << "enter your number:   ";
+//     // cin >> num;
+
+//     // if (num >= 0)
+//     // {
+//     //     if (num % 2 == 0)
+//     //         cout << "Even";
+//     //     else
+//     //         cout << "Odd";
+//     // }
+//     // else
+//     //     cout "Wrong number";
+
+
+
+
+
+
+//     // int grade;
+
+//     // cout << " Enter your grade \t";
+//     // cin >> grade;
+
+//     // if (grade == 100)
+//     //     cout << " your grade is A+";
+
+//     // else if (grade >= 90 && grade < 100)
+//     //     cout << " your grade is A";
+
+//     // else if (grade >= 80 && grade < 90)
+//     //     cout << " your grade is B";
+
+//     // else if (grade >= 70 && grade << 80)
+//     //     cout << " your grade is C";
+
+//     // else if (grade >= 60 && grade << 70)
+//     //     cout << " your grade is D";
+
+//     // else
+//     //     cout << "your grade is F";
+
+
+
+
+
+
+//     // char marital_status , gender;
+//     // int age;
+
+//     // cout << "Enter your marital status    ";
+//     // cin >> marital_status;
+
+//     // cout << "Enter your sex    ";
+//     // cin >> gender;
+
+//     // cout << "Enter your age    ";
+//     // cin >> age;
+
+//     // if(marital_status == 'm' || 
+//     // (marital_status == 's' && gender == 'm' && age > 30) ||
+//     // (marital_status == 's' && gender == 'f' && age > 25) )
+
+//     //     cout << "true";
+//     // else 
+//     //     cout << "fales";
+
+
+
+
+
+
+//     // int i;
+
+//     // cout << "Enter value of I = ";
+//     // cin >> i;
+//     // switch(i)
+//     // {
+//     //     case 10:
+//     //         cout << "I`m in first case";
+//     //         break;
+//     //     case 20:
+//     //         cout << "I`m in second case";
+//     //         break;
+//     //     case 30:
+//     //         cout << "I`m in third case";
+//     //         break;
+//     //     default:
+//     //         cout << "I`m in default case";
+//     // }
+
+
+
+
+
+
+
+
+
+//     // int counter = 1,grade = 0,sum = 0;
+
+//     // while(counter <=6)
+//     // {
+//     //     cout << "Enter grade of student = ";
+//     //     cin>> grade;
+//     //     sum +=grade;
+//     //     counter ++;
+//     // }
+//     // cout << "average grade = " << sum /6;
+
+
+
+
+
+
+
+
+
+
+
+//     // int num = 0, sum = 0;
+
+//     // while(num >= 0)
+//     // {
+//     //     cout << "Enter your postive number = ";
+//     //     cin >> num;
+//     //     sum += num;
+        
+//     // }
+
+//     // cout << "your collection number = " << sum << "\n";
+
+
+
+
+
+
+
+
+
+
+//     // int counter = 1, sum = 0, grade = 0;
+
+//     // do
+//     // {
+//     //     cout << "Enter your grade = ";
+//     //     cin >> grade;
+//     //     sum += grade;
+//     //     counter ++;
+        
+//     // }
+//     // while(counter <= 5 && grade != -1);
+
+//     // cout << "sum = " << sum;
+
+//     // int sum =0, grade =0;
+
+//     // for (int i =1; i <=6; i++) {
+//     //     cout << "Enter your grade: ";
+//     //     cin >> grade;
+//     //     sum += grade;
+//     // }
+//     // cout << "your grade of average = " << float(sum)/6;
+
+//     // int num , sumeven = 0, sumodd =0;
+//     // for (int i = 1; i <=10; i++) {
+//     //     cout << "Enter your numbers: ";
+//     //     cin >> num;
+//     //     if (num % 2 == 0) {
+//     //         cout << num <<": is even number \n";
+//     //         sumeven += num;
+//     //     }
+//     //     else {
+//     //         cout << num <<": is odd number \n";
+//     //         sumodd += num;
+//     //     }
+//     // }
+//     // cout <<"collection of even numbers is: " << sumeven << endl;
+//     // cout <<"collection of odd numbers is: " << sumodd << endl;
+
+//     // int num , fact = 1;
+//     // cout << "Enter your number: ";
+//     //     cin >> num;
+//     // if (num <0)
+//     //     cout << "Enter postive number only";
+//     // else {
+//     //     for (int i = 1; i <= num;i++)
+//     //     fact = fact*i;
+
+//     // cout << "your Factorial = " << fact;
+//     // }
+
+
+
+//     // int fact =1;
+//     // for (int num = 1;num <=10;num++) {
+//     //     if (num < 0)
+//     //         cout << "Enter postive number only \n";
+//     //     else {
+//     //         for (int i =1;i <=num;i++)
+//     //             fact = fact*i;
+
+//     //             cout << "the factorial of " << num << " = "<<fact << endl;
+//     //     }
+//     // }
+
+
+//     // for (int line = 1; line <=10;line++) {
+
+//     //     for(int star = 1;star <=line;star++)
+//     //         cout << "*";
+
+//     //     cout <<endl;
+//     // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // int i = -5;
+//     // double x = 5,y = 2;
+
+//     // cout << "the absolute value is: "<< abs(i) << endl;
+//     // cout << "the power of value is: " << pow(x,y) <<endl;
+
+    
+
+//     // return 0;
+
+// }
+    // int larger(int, int);
+    // const float pi = 3.14;
+    // float area (int r);
+    // float volume (int r);
+
+//     int sum(int n1,int n2,int n3 = 100);
+//     float average(int n1, int n2, int n3);
+// int main()
+// {
+    // int n1,n2;
+
+    // cout << "Enter two numbers to calculate the larger number. \n";
+    // cout << "first number: ";
+    // cin>>n1;
+    // cout << "second number: ";
+    // cin>>n2;
+
+    // cout << "The larger number is: "<< larger(n1,n2)<<endl;
+
+
+    // int r;
+
+    // cout <<"Enter your radius: ";
+    // cin>>r;
+
+    // cout <<"your Area is: "<<area(r)<<endl;
+    // cout<<"your volume is: "<<volume(r)<<endl;
+
+
+
+
+
+
+
+    // float n1,n2,n3;
+    // cout << "Enter 3 numbers: " <<" "<< " "<<endl;
+    // cin >>n1>>n2>>n3;
+    // cout <<"Average of 3 numbers is: "<<average(n1,n2,n3);
+
+
+
+
+
+
+
+
+
+
+// }
+
+
+
+// float area(int r)
+// {
+//     return 4 * pi * pow(r,2);
+// }
+
+// float volume(int r)
+// {
+//     return 4/3 * pi * pow(r,3);
+// }
+
+// int larger(int n1,int n2)
+// {
+//     if(n1>n2)
+//         return n1;
+//     else
+//         return n2;
+// }
+
+
+
+// int sum(int n1,int n2,int n3)
+// {
+//     return n1 + n2 + n3;
+// }
+
+// float average(int n1, int n2, int n3)
+// {
+//     return sum(n1, n2, n3)/3;
+// }
+
+
+
+
+
+
+
+
+
+
+// void swap(int &x,int &y)
+// {
+//     int temp;
+
+//     temp = x;
+//     x = y;
+//     y = temp;
+// }
+
+// int main()
+// {
+//     int i = 10,j =50;
+    
+//     swap(i,j);
+//     cout << "i becomes: "<< i <<endl;
+//     cout << "j becomes: "<< j <<endl;
+    
+// }
+
+// int main()
+// {
+    // float n1,n2;
+    // char op;
+    // int i,y = 0;
+
+    // while(y <=2)
+    // {
+    //     cout <<"Enter your first number: ";
+    //     cin>>n1;
+    // cout <<"Enter your second number: ";
+    //     cin>>n2;
+    // cout <<"1.(+) \t 2.(-) \t 3.(*) \t 4.(/)"<<endl;
+    // cout <<"Enter your operator: ";
+    //     cin>>op;
+    
+    // if(op == '1')
+    //     cout <<"Result is: "<<n1+n2<<endl;
+    // else if(op == '2')
+    //     cout <<"Result is: "<<n1-n2<<endl;
+    // else if(op == '3')
+    //     cout <<"Result is: "<<n1*n2<<endl;
+    // else if(op == '4')
+    //     cout <<"Result is: "<<n1/n2<<endl;
+    // else
+    //     cout <<"your operator is wrong"<<endl;
+
+    // cout<< "do u want to calculata again ? "<<endl;
+    // cout<<"1.(Yes) \t 2.(No)"<< endl;
+    //     cin>>y;
+    // if(y==1)
+    //     cout <<"thank you" <<endl;
+    // else if(y==2)
+    //     cout<< "good luck"<<endl;
+    // else
+    //     cout<<"error";
+    
+    // i++;
+    // }
+    
+// }
+
+
+
+
+int main()
+{
+    int x;
+
+    cout << "Enter your grade: ";
+    cin >> x;
+
+    switch(x)
+    {
+        case 0 ... 50: 
+            cout << "F\n";
+            break;
+        case 51 ... 60: 
+            cout << "E\n";
+            break;
+        case 61 ... 70: 
+            cout << "D\n";
+            break;
+        case 71 ... 80: 
+            cout << "C\n";
+            break;
+        case 81 ... 90: 
+            cout << "B\n";
+            break;
+        case 91 ... 100: 
+            cout << "A\n";
+            break;
+
+            default: 
+                cout << "Error \n";
+                break;
+    }
+}
